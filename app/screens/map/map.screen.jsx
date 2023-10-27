efcdcadb3a6b851d670faf3d8602c2bd1052c007
J_Tomko <jrt5632@psu.edu>
2023-10-27 12:42:33 -0400

    prints location of phone

--- app/screens/map/map.screen.jsx
+++ app/screens/map/map.screen.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
 import Text from '../../../appStyles/customStyle'
+import { location as Location } from "../../location/location";
 
-export const Map = () => {
+export const Map = ({navigation}) => {
     
     return(
         <SafeAreaView>
             <Text>Map page</Text>
+            <Location/>
         </SafeAreaView>
     )
 }
