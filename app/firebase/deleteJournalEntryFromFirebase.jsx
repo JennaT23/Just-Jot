11dbfe863346ae3a9ff8f7cfb9440206ef31949f
amedawg <83100446+amedawg@users.noreply.github.com>
2023-12-03 06:01:44 -0500

    DELETE JOURNAL ENTRY IS BROKEN SEND HELP

--- app/firebase/deleteJournalEntryFromFirebase.jsx
+++ app/firebase/deleteJournalEntryFromFirebase.jsx
@@ -1,24 +1,33 @@
-import { getFirestore, getStorage, doc, getDoc, deleteDoc } from "firebase/firestore";
+import { getFirestore, doc, getDoc, deleteDoc } from "firebase/firestore";
+import { getStorage, ref, deleteObject } from "firebase/storage";
 import { Alert } from "react-native";
 
 export const deleteJournalEntryFromFirebase = async (entryId) => {
-    try {
-        const db = getFirestore();
-        const storage = getStorage();
+  try {
+    const db = getFirestore();
+    const storage = getStorage();
 
     const entryRef = doc(db, "JournalEntries", entryId);
     const entrySnapshot = await getDoc(entryRef);
 
     // making sure the actual entry exists
     if (entrySnapshot.exists()) {
+      const image = entrySnapshot.data().Images;
+
+      // check if the entry had an image before deletion
+      if (image) {
+        const imageRef = ref(storage, image);
+        await deleteObject(imageRef);
+      }
+
       await deleteDoc(entryRef);
 
-      Alert.alert('Entry Deleted');
+      Alert.alert("Entry Deleted");
     } else {
-      Alert.alert('Entry does not exist');
+      Alert.alert("Entry does not exist");
     }
   } catch (error) {
-    console.error('Error deleting journal entry:', error);
+    console.error("Error deleting journal entry:", error);
     throw error;
   }
 };
